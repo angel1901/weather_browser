df916d02f5e0793f91e9aebce45ab28eecafa833
adra-bt <111783710+adra-bt@users.noreply.github.com>
2023-06-13 07:45:59 -0500

    New marker

--- resources/js/Components/MarkerIcon.jsx
+++ resources/js/Components/MarkerIcon.jsx
@@ -1,6 +1,8 @@
 import L from "leaflet";
 
+import IconMarker from "public/images/marker-icon.png";
+
 export const MarkerIcon = L.icon({
-    iconUrl: require("../node_modules/leaflet/dist/images/marker-icon.png"),
+    iconUrl: IconMarker,
     // shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
